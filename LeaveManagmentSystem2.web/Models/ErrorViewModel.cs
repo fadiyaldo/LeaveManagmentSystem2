07d9a171a19274ec8895f8c6e677d82b47096c76
fadi <Fadi_baza@yahoo.com>
2025-03-01 14:52:33 -0500

    Add project files.

---
+++ LeaveManagmentSystem2.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagmentSystem2.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
