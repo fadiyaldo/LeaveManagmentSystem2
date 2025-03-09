711aab19e3906c0dc035327eecab8c531b3604f9
fadi <Fadi_baza@yahoo.com>
2025-03-09 15:15:00 -0400

    complete leave managment module

--- LeaveManagmentSystem2.web/Data/ApplicationDbContext.cs
+++ LeaveManagmentSystem2.web/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace LeaveManagmentSystem2.web.Data;
 
@@ -7,6 +8,8 @@ public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
-    {
-    }
+    { }
+
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
 }
